7b26c4a3b144ee6ef1912ce48018de2f908a3265
patyhank <41978811+patyhank@users.noreply.github.com>
2019-05-22 20:30:58 +0800

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ fs.readdir("./commands/", (err, files) => {
     client.commands.set(commandName, props);
   });
 });
-var date = new Date().setHours(20, 50, 0, 0);
+var date = new Date().setHours(20, 35, 0, 0);
 new Date() > date && send();
   var channel = message.guild.channels.find('id', '566218339769253898');
   var send = channel.send('~~－－－－－－－－－－－－－－－－－－－~~\n@《管理員》 @《官方人員》**簽到時刻**\n1. 請每日簽到\n2. 三日以上沒簽會扣50分\n3. 三日以上不在，要請假，密無名。\n4. 點選勾勾進行反應\n5. 昨天沒簽到可以補簽(限5天內)\n~~－－－－－－－－－－－－－－－－－－－~~')
