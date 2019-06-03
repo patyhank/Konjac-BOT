b8be083565dff0c9f274c9c3fa6ed7bf48220b40
patyhank <mlpmmy@gmail.com>
2019-06-03 19:34:17 +0800

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -8,39 +8,7 @@ module.exports.run = async (bot, message, args) => {
   //if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(":no_entry:  你沒有權限");
   //if(!message.member.roles.find("name", "「副服主」co-owner") || !message.member.roles.find("name", "「服主」Owner ")){
   //message.reply(':no_entry: 你沒有權限')
-  const user = message.mentions.users.first();
-  // If we have a user mentioned
-  if (user) {
-    // Now we get the member from the user
-    const member = message.guild.member(user);
-    // If the member is in the guild
-    if (member) {
-      /**
-       * Ban the member
-       * Make sure you run this on a member, not a user!
-       * There are big differences between a user and a member
-       * Read more about what ban options there are over at
-       * https://discord.js.org/#/docs/main/stable/class/GuildMember?scrollTo=ban
-       */
-      member.ban({
-        reason: 'They were bad!',
-      }).then(() => {
-        // We let the message author know we were able to ban the person
-        message.reply(`Successfully banned ${user.tag}`);
-      }).catch(err => {
-        // An error happened
-        // This is generally due to the bot not being able to ban the member,
-        // either due to missing permissions or role hierarchy
-        message.reply('I was unable to ban the member');
-        // Log the error
-        console.error(err);
-      });
-    } else {
-      // The mentioned user isn't in this guild
-      message.reply('That user isn\'t in this guild!');
-    }
-  } else {
-  // Otherwise, if no user was mentioned
-    message.reply('You didn\'t mention the user to ban!');
-  }
+  var banuser = message.mentions.users.first().id
+  message.guild.ban(banuser);
+
 }
