b3f20838266d92fffc16979abba1743876db4d4d
patyhank <mlpmmy@gmail.com>
2019-06-03 19:36:15 +0800

    Update vote.js

--- commands/vote.js
+++ commands/vote.js
@@ -4,8 +4,6 @@ module.exports.run = async (bot, message, args) => {
   if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(":no_entry:  你沒有權限");
   let vote = args.join(" ");
   let channel = message.guild.channels.find(channel => channel.name === '投票');
-  if (message.guild.id = "543993456310222877") return true;
-  let channel = message.guild.channels.find(channel => channel.id === '585031577944457216');
   const vo = new Discord.RichEmbed()
     .setColor('GREEN')
     .addField("投票:", `${vote}`);
