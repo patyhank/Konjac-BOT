3cee2b733b3e448192bd9b9a1fdad276102f71b8
patyhank <mlpmmy@gmail.com>
2019-06-03 19:23:31 +0800

    update kick

--- commands/vote.js
+++ commands/vote.js
@@ -1,14 +1,16 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
-    message.delete("1");
-    if(message.member.id != "276337222196658178") return message.channel.send(":no_entry:  你沒有權限");
-    let vote = args.join(" ");
-      let channel = message.guild.channels.find(channel => channel.name === '投票');
-      const vo = new Discord.RichEmbed()
-      .setColor('GREEN')
-      .addField("投票:", `${vote}`);
-      channel.send(vo)
-      .then(msg => {
+  message.delete("1");
+  if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(":no_entry:  你沒有權限");
+  let vote = args.join(" ");
+  let channel = message.guild.channels.find(channel => channel.name === '投票');
+  if (message.guild.id = "543993456310222877") return true;
+  let channel = message.guild.channels.find(channel => channel.id === '585031577944457216');
+  const vo = new Discord.RichEmbed()
+    .setColor('GREEN')
+    .addField("投票:", `${vote}`);
+  channel.send(vo)
+    .then(msg => {
       msg.react('✅')
       msg.react('❎')
     });
