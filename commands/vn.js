23605c0758bae1235ddf2d5948ecb2e516048b36
蒟蒻 <41978811+patyhank@users.noreply.github.com>
2019-06-15 17:26:24 +0800

    Update vn.js

--- commands/vn.js
+++ commands/vn.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 module.exports.run = async (client, message, args) => {
       message.delete();
       setTimeout(() => {
-        var nick = message.member.displayName().split(/-+/g);
+        var nick = message.member.displayName().split(/ +/g);
         console.log(nick)          
       }, 20);
 };
