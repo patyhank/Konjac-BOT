3508987486bb73e309f02712e3df4d818d86883d
蒟蒻 <41978811+patyhank@users.noreply.github.com>
2019-06-15 18:34:29 +0800

    update something

--- commands/vn.js
+++ commands/vn.js
@@ -1,9 +1,25 @@
 const Discord = require("discord.js");
+const mine
+var request = require('request');
 module.exports.run = async (client, message, args) => {
       message.delete();
       setTimeout(() => {
-        var nick1 = message.member.nickname()
-        var nick0 = nick1.split(/ +/g);
-        console.log(nick1)     
+        var ign = args[0]
+        var options = {
+            uri: 'https://api.mojang.com/profiles/minecraft',
+            method: 'POST',
+            json: [`${ign}`]
+          };
+          
+          request(options, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+              console.log(body)
+              if(body.hasOwnProperty("name")){
+              message.member.addRole('589106886398312448')
+              message.member.removeRole('589375747433693194')
+              message.member.setNickname(message.content)
+            }
+            }
+          });
       }, 20);
 };
