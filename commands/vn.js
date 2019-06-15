cff507ecb66becdb24152036b654300c2b1930fa
蒟蒻 <41978811+patyhank@users.noreply.github.com>
2019-06-15 17:26:48 +0800

    Update vn.js

--- commands/vn.js
+++ commands/vn.js
@@ -3,6 +3,8 @@ module.exports.run = async (client, message, args) => {
       message.delete();
       setTimeout(() => {
         var nick = message.member.displayName().split(/ +/g);
-        console.log(nick)          
+        console.log(nick)       
+        var nick1 = message.member.nickname().split(/ +/g);
+        console.log(nick1)     
       }, 20);
 };
