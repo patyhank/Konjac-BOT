8e2d24892dc708d4678f2c89f9bc525c475274ab
蒟蒻 <41978811+patyhank@users.noreply.github.com>
2019-06-15 17:24:11 +0800

    Update vn.js

--- commands/vn.js
+++ commands/vn.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js");
 module.exports.run = async (client, message, args) => {
       message.delete();
-      message.channels.send("請稍後...")
       setTimeout(() => {
         var nick = message.member.displayName().split(/-+/g);
         console.log(nick)          
